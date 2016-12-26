8ba26e9e358b4be26c6c936dc97b56b50c758ed7
Felix Hassemer <felixhassemer@gmail.com>
2016-12-26 12:32:47 +0100

    few modifications

--- sketch_161114b_towers/build/source/sketch_161114b_towers.java
+++ sketch_161114b_towers/build/source/sketch_161114b_towers.java
@@ -14,38 +14,22 @@ import java.io.IOException;
 
 public class sketch_161114b_towers extends PApplet {
 
-int count = 0;
-float[] xArray;
-float[] yArray;
+// VARIABELN
+int x, y;
+
 
 public void setup() {
   
   background(255);
-  noStroke();
-  fill(0);
-  frameRate(5);
+  stroke(0);
+  strokeWeight(3);
 }
 
 public void draw() {
-  if (count % 10 == 1) {
-    background(255);
-    for (int i = 0; i < 10; i += 1) {
-      for (int j = 0; j < 10; j += 1) {
-        float x = random(width);
-        float y = random(height);
-        xArray[i] = x;
-        yArray[j] = y;
-        ellipse(xArray[i], yArray[j], 100, 100);
-      }
-    }
-  //   for (int i = 0; i < 10; i += 1) {
-  //     float x = random(width);
-  //     float y = random(height);
-  //     ellipse(x, y, 100, 100);
-  //   }
-  }
-  count++;
-  // println(count%10);
+  background(255);
+  x = PApplet.parseInt(random(width));
+  y = PApplet.parseInt(random(height));
+  ellipse(x, y, 100, 100);
 }
   public void settings() {  size(1000, 1000); }
   static public void main(String[] passedArgs) {
--- sketch_161114b_towers/sketch_161114b_towers.pde
+++ sketch_161114b_towers/sketch_161114b_towers.pde
@@ -1,33 +1,17 @@
-int count = 0;
-float[] xArray;
-float[] yArray;
+// VARIABELN
+int x, y;
+
 
 void setup() {
   size(1000, 1000);
   background(255);
-  noStroke();
-  fill(0);
-  frameRate(5);
+  stroke(0);
+  strokeWeight(3);
 }
 
 void draw() {
-  if (count % 10 == 1) {
-    background(255);
-    for (int i = 0; i < 10; i += 1) {
-      for (int j = 0; j < 10; j += 1) {
-        float x = random(width);
-        float y = random(height);
-        xArray[i] = x;
-        yArray[j] = y;
-        ellipse(xArray[i], yArray[j], 100, 100);
-      }
-    }
-  //   for (int i = 0; i < 10; i += 1) {
-  //     float x = random(width);
-  //     float y = random(height);
-  //     ellipse(x, y, 100, 100);
-  //   }
-  }
-  count++;
-  // println(count%10);
+  background(255);
+  x = int(random(width));
+  y = int(random(height));
+  ellipse(x, y, 100, 100);
 }
